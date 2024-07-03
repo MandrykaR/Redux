3828a98262760449b0d68dedac6f3ee6d1d4f251
MandrykaR <mandrykaroman01@gmail.com>
2024-07-03 13:52:39 +0200

    l2-t1

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -11,7 +11,7 @@ export const decrement = () => {
   return { type: DECREMENT };
 };
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
@@ -22,9 +22,4 @@ export const counterReducer = (state = 0, action) => {
   }
 };
 
-const store = createStore(counterReducer);
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
+export const store = createStore(counterReducer);
