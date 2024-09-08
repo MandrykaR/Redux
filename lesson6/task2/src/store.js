70183c2b9ab5d6b286115a90a8571f6bbeec51f0
MandrykaR <mandrykaroman01@gmail.com>
2024-09-08 15:58:40 +0200

    l6-t2

--- lesson6/task2/src/store.js
+++ lesson6/task2/src/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 import usersReducer from './users/users.reducer';
 
-// const logger = (store) => (next) => (action) => {
-//   console.group(action.type);
-//   console.info('dispatching', action);
-//   let result = next(action);
-//   console.log('new state', store.getState());
-//   console.groupEnd();
-//   return result;
-// };
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('new state', store.getState());
+  console.groupEnd();
+  return result;
+};
 
 const reducer = combineReducers({
   users: usersReducer,
@@ -17,6 +17,9 @@ const reducer = combineReducers({
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 export default store;
